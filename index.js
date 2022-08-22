16d25cf4ab309eff9a6cb86f22f4e95efd1b267f
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-08-22 23:41:10 +0100

    working for get/delete
    still have post and update to do

---
+++ index.js
@@ -0,0 +1,119 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(express.json());
+const PORT = 3001;
+
+// ********** Content ************
+let persons = [
+	{
+		"id": 1,
+		"name": "Arto Hellas",
+		"number": "040-123456",
+	},
+	{
+		"id": 2,
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523",
+	},
+	{
+		"id": 3,
+		"name": "Dan Abramov",
+		"number": "12-43-234345",
+	},
+	{
+		"id": 4,
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122",
+	},
+];
+
+// ********** Handlers ************
+
+// Load landing page
+app.get("/", (req, res) => {
+	res.send(
+		"<h1>Welcome to the Phone Book</h2><a href='/info'><span>Info Page</span></a>"
+	);
+});
+
+// Load info page
+app.get("/info", (req, res) => {
+	let totalPersons = persons.length;
+	let timeNow = new Date();
+	res.send(
+		`<h1>Welcome to the Phone Book</h2> <span>Phonebook has info for ${totalPersons} people</span><br/><span>${timeNow}</span></span><br/><a href='/'><span>Home</span></a>`
+	);
+});
+
+// Handle request for a single record, n.b. must be a number to match the type of the key in the db
+app.get("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	console.log(id);
+	const person = persons.find((person) => person.id === id);
+	console.log(person);
+	if (person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+});
+
+// Return the whole list
+app.get("/api/persons", (req, res) => {
+	res.json(persons);
+});
+
+// Delete a single entry, this works by re-filtering the list and excluding the matching id.
+// Works since persons is an array of objects.
+
+app.delete("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	persons = persons.filter((person) => person.id !== id);
+	res.status(204).end();
+});
+
+app.post("/api/persons/", (req, res) => {
+	const body = req.body;
+
+	// function to generate a new random id number as requested
+	const newId = function () {
+		return Math.ceil(Math.random() * 10000 + persons.length);
+	};
+
+	// error message if empty,  calling the return stops the function here
+	if (!body.content) {
+		return res.status(400).json({
+			error: "content missing",
+		});
+	}
+
+	if (persons.find((person) => body.name === person.name)) {
+		return res.status(410).json({
+			error: "duplicate name",
+		});
+	}
+
+	// throw an error if no name or number, else standardise format of the output and
+	if (!body.name || !body.number) {
+		return res.status(420).json({
+			error: "missing name or number",
+		});
+	} else {
+		const person = {
+			id: newId(),
+			name: body.name,
+			number: body.number,
+		};
+
+		persons.push(person);
+
+		res.json(person);
+	}
+});
+
+// ********** Server ************
+
+app.listen(PORT);
+console.log(`Server running on port: ${PORT}`);
